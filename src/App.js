f7fa8d45c4dfd8f3364d9f852cfed379bfba349b
x0r3k <kir221dol@gmail.com>
2020-12-12 22:05:58 +0200

    react + redux config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+// import Router from './routing';
+import configureStore from './redux';
+
+const store = configureStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Provider store={store}>
+
+      </Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+export default function configureStore(preloadedState) {
+  const middlewareEnhancer = applyMiddleware(thunkMiddleware);
+
+  const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+  return store;
+}
